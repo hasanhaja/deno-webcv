4edcbfbb3c315fbd295d29089307e74332b1d020
Hasan Ali <hasanhaja@gmail.com>
2022-07-19 22:52:15 +0100

    Implement simple home page and responsive navbar

--- islands/NavBar.tsx
+++ islands/NavBar.tsx
@@ -6,12 +6,14 @@ import { tw } from "@twind";
 const NavLink = (
   { href, children }: { href: string; children: ComponentChildren },
 ) => (
-  <a
-    class={tw`text-2xl hover:underline md:text-3xl`}
-    href={href}
-  >
-    {children}
-  </a>
+  <li class={tw`flex justify-end`}>
+    <a
+      class={tw`text-2xl hover:underline md:text-3xl p-1 pr-4`}
+      href={href}
+    >
+      {children}
+    </a>
+  </li>
 );
 // TODO At the md breakpoint, the nav list should collapse
 // Three size: normal, md and xl
@@ -20,23 +22,34 @@ const NavBar = () => {
   const [expanded, setExpanded] = useState(false);
   return (
     <Fragment>
-      <button aria-controls="primary-navigation" aria-expanded={expanded}>
+      <button
+        class={tw`md:hidden z-[9999] absolute top-8 right-8`}
+        onClick={() => setExpanded((prev) => !prev)}
+        aria-controls="primary-navigation"
+        aria-expanded={expanded}
+      >
         <span class={tw`sr-only`}>Menu</span>
+        <img
+          class={tw`w-10 h-10`}
+          src={expanded ? "/close-menu.svg" : "/hamburger-menu.svg"}
+          alt={expanded ? "Close menu" : "Open menu"}
+        />
       </button>
-      <nav class={tw`md:flex md:items-center md:justify-between`}>
+      <nav
+        class={tw
+          `md:flex md:items-center md:justify-between mb-2 bg-green-300 rounded-md md:bg-transparent`}
+      >
         <ul
-          class={tw`md:flex md:items-center md:gap-2`}
+          class={tw`md:flex md:items-center md:gap-2 ${
+            expanded ? "block" : "hidden"
+          }`}
           id="primary-navigation"
+          aria-hidden={!expanded}
+          hidden={!expanded}
         >
-          <li>
-            <NavLink href="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink href="/weather">Weather</NavLink>
-          </li>
-          <li>
-            <NavLink href="/count">Counter</NavLink>
-          </li>
+          <NavLink href="/">Home</NavLink>
+          <NavLink href="/weather">Weather</NavLink>
+          <NavLink href="/count">Counter</NavLink>
         </ul>
       </nav>
     </Fragment>
